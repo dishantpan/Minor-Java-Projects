2ec375aaa5a01b460050924c2b9b07647c5cf96b
Dishant Panchal <155050627+dishantpan@users.noreply.github.com>
2025-12-30 23:15:19 +0530

    Add Project-03: Library Management System source code

---
+++ Project-03-Library-Management-System/src/Book.java
@@ -0,0 +1,44 @@
+// Java Program to Illustrate book Class that
+// takes Input from the books and related information
+
+package College;
+
+// Importing required classes
+import java.util.Scanner;
+
+// Class
+public class Book {
+
+    // Class data members
+    public int sNo;
+    public String BookName;
+    public String authorName;
+    public int BookQty;
+    public int BookQtyCopy;
+
+    // Creating object of Scanner class to
+    // read input from users
+    Scanner input = new Scanner(System.in);
+
+    // Method
+    // To add book details
+    public Book()
+    {
+        // Display message for taking input later
+        // taking input via
+        // nextInt() and nextLine() standard methods
+        System.out.println("Enter Serial No of Book:");
+        this.sNo = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Enter Book Name:");
+        this.bookName = input.nextLine();
+
+        System.out.println("Enter Author Name:");
+        this.authorName = input.nextLine();
+
+        System.out.println("Enter Quantity of Books:");
+        this.bookQty = input.nextInt();
+        bookQtyCopy = this.bookQty;
+    }
+}
