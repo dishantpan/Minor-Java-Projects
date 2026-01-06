72cd68bc269da451ff1831eaf6528d16ce14c3b5
Dishant Panchal <155050627+dishantpan@users.noreply.github.com>
2026-01-06 13:01:54 +0530

    Create Item.java

---
+++ PROJECT-05: INVENTORY MANAGEMENT SYSTEM/src/com/inventory/Item.java
@@ -0,0 +1,38 @@
+package com.inventory;
+
+public class Item {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Item(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{name='" + name + "', quantity=" + quantity + ", price=" + price + "}";
+    }
+}
